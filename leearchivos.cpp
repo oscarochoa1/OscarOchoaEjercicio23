ea73cdd7ca4afc88bab1867f52b230d1af1e3bd1
oscarochoa1 <46940987+oscarochoa1@users.noreply.github.com>
2019-04-09 08:39:44 -0500

    Update leearchivos.cpp

--- leearchivos.cpp
+++ leearchivos.cpp
@@ -3,11 +3,15 @@
 using namespace std;
  
 void ejemplo_lee(string filename);
+void ejemplo_leenew(string filenamenew);
 
 
 int main () {
   string filename;
+    string filenamenew;
   filename = "valores_x.txt";
+    filenamenew = "valores_y.txt";
+    ejemplo_leenew(filenamenew);
   ejemplo_lee(filename);
 
   
@@ -20,6 +24,21 @@ void ejemplo_lee(string filename){
   infile.open(filename); 
   
   cout << "Leyendo de " << filename << endl; 
+  getline(infile, line);
+  while(infile){
+    cout << line << endl;
+    getline(infile, line);
+  }
+
+  infile.close();
+}
+void ejemplo_leenew(string filenamenew){
+  ifstream infile; 
+  string line;
+
+  infile.open(filenamenew); 
+  
+  cout << "Leyendo de " << filenamenew << endl; 
   getline(infile, line);
   while(infile){
     cout << line << endl;
