951cd78d86be26b8b0cfdf86bdecc26c92562847
oscarochoa1 <46940987+oscarochoa1@users.noreply.github.com>
2019-04-09 07:35:46 -0500

    Create lo de por si aca.cpp

---
+++ lo de por si aca.cpp
@@ -0,0 +1,48 @@
+##PARA LEER ARCHIVOS
+
+#include <fstream>
+#include <iostream>
+using namespace std;
+ 
+void ejemplo_lee(string filename);
+void ejemplo_escribe(string filename);
+
+int main () {
+  string filename;
+  filename = "valores_x.txt";
+  ejemplo_lee(filename);
+
+  filename = "valores_x.txt";
+  ejemplo_escribe(filename);
+  return 0;
+}
+
+void ejemplo_lee(string filename){
+  ifstream infile; 
+  string line;
+
+  infile.open(filename); 
+  
+  cout << "Leyendo de " << filename << endl; 
+  getline(infile, line);
+  while(infile){
+    cout << line << endl;
+    getline(infile, line);
+  }
+
+  infile.close();
+}
+
+void ejemplo_escribe(string filename){
+  ofstream outfile;
+
+
+  outfile.open(filename);
+
+  cout << "Escribiendo en " << filename << endl; 
+  for (int i=0; i < 10; i++){
+    outfile << i << endl;
+  }
+  outfile.close(); 
+}
+
